e806c324176566f5ea652579031d1227db01a265
BobMuscarella <bob.muscarella@gmail.com>
2020-07-19 18:31:49 +0200

    update code and remove trailing lines from survival data

--- Code/Combined.R
+++ Code/Combined.R
@@ -1,1 +1,26 @@
 ### Combined code for Seedling Drought Experiment
+
+
+
+# Read data from Github
+
+path <- "https://raw.github.com/bobmuscarella/Luquillo_LTER_Seedling_Drought_Experiment/master/Data/"
+
+growth_url <- paste0(path, "LUQ_DroughtExp_Seedling_growth.csv")
+survive_url <- paste0(path, "LUQ_DroughtExp_Seedling_survival.csv")
+traits_url <- paste0(path, "LUQ_DroughtExp_Seedling_traits.csv")
+photo_url <- paste0(path, "LUQ_DroughtExp_Survey_photosynthesis.csv")
+
+growth <- read.csv(growth_url)
+survival <- read.csv(survive_url)
+traits <- read.csv(traits_url)
+photo <- read.csv(photo_url)
+
+
+
+
+
+
+
+
+
