ed774d5ad31010747764159535b0a17074b36ea5
BobMuscarella <bob.muscarella@gmail.com>
2020-07-21 14:53:33 +0200

    update combined code

--- Code/Combined.R
+++ Code/Combined.R
@@ -1,145 +1,79 @@
-### Combined code for Seedling Drought Experiment
+###########################################################################
+###########################################################################
+###########################################################################
+### Combined code for Seedling Drought Experiment #########################
+###########################################################################
+###########################################################################
+###########################################################################
+
+#####################
+### General setup ###
+#####################
 
 # Read data from Github
 path <- "https://raw.github.com/bobmuscarella/Luquillo_LTER_Seedling_Drought_Experiment/master/Data/"
 
-growth_url <- paste0(path, "LUQ_DroughtExp_Seedling_growth.csv")
-survive_url <- paste0(path, "LUQ_DroughtExp_Seedling_survival.csv")
-traits_url <- paste0(path, "LUQ_DroughtExp_Seedling_traits.csv")
-photo_url <- paste0(path, "LUQ_DroughtExp_Survey_photosynthesis.csv")
+grow <- read.csv(paste0(path, "LUQ_DroughtExp_Seedling_growth.csv"))
+surv <- read.csv(paste0(path, "LUQ_DroughtExp_Seedling_survival.csv"))
+trait <- read.csv(paste0(path, "LUQ_DroughtExp_Seedling_traits.csv"))
+photo <- read.csv(paste0(path, "LUQ_DroughtExp_Survey_photosynthesis.csv"))
 
-grow <- read.csv(growth_url)
-surv <- read.csv(survive_url)
-trait <- read.csv(traits_url)
-photo <- read.csv(photo_url)
+# Load packages
+require(survival)
+require(survminer)
+require(coxme)
+require(ggplot2)
+require(RColorBrewer)
+library(lme4)
+
+# Set a color palette for species
+cols <- brewer.pal(8, "Dark2")
 
 
 #########################
 ### Survival analysis ###
 #########################
 
-library(survival)
-library(survminer)
-library(coxme)
-library(ggplot2)
 
 ### Run species-by-species survival models in a loop
-surv_results <- list()
+surv_fits <- surv_fits_plot <- list()
+options(na.action=na.exclude) # retain NA in predictions
+
 for (i in seq_along(levels(surv$Species))){
   # Subset the data for the 'i-th' species
   focdat <- surv[surv$Species %in% levels(surv$Species)[i],]
-  # Run the models (we had experimented with other random effects but AIC selected this)
-  surv_results[[i]] <- coxme(Surv(Days, Status) ~ Moisture + Densiometer + May_LA 
+  # Run the models with plot random effect (we tested other random effects but AIC selected this)
+  surv_fits[[i]] <- coxme(Surv(Days, Status) ~ Moisture + Densiometer + May_LA
                              + (1|Plot), data=focdat)
-  names(surv_results)[i] <- levels(surv$Species)[i]
+  # Run models without random effect in order to be able to plot predicted response
+  # (not currently possible with coxme models)
+  surv_fits_plot[[i]] <- coxph(Surv(Days, Status) ~ Moisture + Densiometer + May_LA, 
+                             data=focdat, model=T)
+  # Name the items in the resulting list
+  names(surv_fits)[i] <- levels(surv$Species)[i]
 }
 
-# Build a summary table for survival model results
-surv_results
+### Function to extract summary table from Cox ME models
+extract_coxme_table <- function (mod){
+  beta <- mod$coefficients
+  nvar <- length(beta)
+  nfrail <- nrow(mod$var) - nvar
+  se <- sqrt(diag(mod$var)[nfrail + 1:nvar])
+  z <- round(beta/se, 2)
+  p <- signif(1 - pchisq((beta/se)^2, 1), 2)
+  table = data.frame(cbind(beta, se, z, p))
+  return(table)
+}
 
+### Make a summary table for survival model results
+t1 <- do.call(rbind, lapply(surv_fits, extract_coxme_table))
+t1 <- round(t1, 3)
+t1$Species <- unlist(lapply(strsplit(rownames(t1), "\\."), function(x)x[[1]]))
+t1$Variable <- unlist(lapply(strsplit(rownames(t1), "\\."), function(x)x[[2]]))
+t1 <- cbind(t1, round(do.call(rbind, lapply(surv_fits, confint)),3))
+t1 <- t1[,c("Species","Variable","beta","2.5 %", "97.5 %","se","z","p")]
 
-
-
-################## Graphing ###########################
-PREMON_test.cox <- predict(surv_results$PREMON)
-
-# The following causes the error (x and y lengths differ)
-plot(surv$Moisture[surv$Species %in% "PREMON"], PREMON_test.cox)
-
-# This is because observations with NA for any co-variate are excluded when fitting the model
-# Some rows have NA for the initial size (May_LA) variable
-# and that gives you the 'unequal' lengths error
-
-# You can remove those records and then look at it
-PREMON_moist <- surv$Moisture[surv$Species %in% "PREMON" & !is.na(surv$May_LA)]
-
-
-UREBAC_moist <- seedlingdata$Moisture[seedlingdata$species %in% "UREBAC" & 
-                                        !is.na(seedlingdata$May_LA)]
-
-MANBID_moist <- seedlingdata$Moisture[seedlingdata$species %in% "MANBID" & 
-                                        !is.na(seedlingdata$May_LA)]
-
-TETBAL_moist <- seedlingdata$Moisture[seedlingdata$species %in% "TETBAL" & 
-                                        !is.na(seedlingdata$May_LA)]
-INLAU_moist <- seedlingdata$Moisture[seedlingdata$species %in% "INLAU" & 
-                                       !is.na(seedlingdata$May_LA)]
-GUAGUI_moist <- seedlingdata$Moisture[seedlingdata$species %in% "GUAGUI" & 
-                                        !is.na(seedlingdata$May_LA)]
-##BOB QUESTION: INSTEAD OF THE LINEAR PREDICTOR I THINK IT MAKES MORE SENSE TO 
-## GET THE SURVIVAL PROBABILITY, WHICH ACCORDING TO THE HELP PAGE WE GET BY 
-## type='expected' AND THEN ITS exp(-expected), WHICH I TRIED OUT BELOW AND IT WORKED
-## THE Y-AXIS UNITS MAKE SENSE (SURVIVAL PROB SHOULD BE 0-1) AND THE OVERALL SHAPE MAKES SENSE
-
-pred <- predict(results$PREMON$RE_none, type='expected')
-Pred<- exp(-pred)
-plot(moist, Pred)
-#plot(moist, pred)
-
-##BOB QUESTION: BUT WHEN I TRY TO DO THE SAME THING WHEN PREDICTING TO 
-## NEW DATA IT PRODUCES A GRAPH THAT DOESN'T MAKE SENSE (Y AXIS DOESN'T MAKE SENSE AND SHAPE SEEMS WEIRD),
-## NOT SURE IF THE type='expected' IS IN THE CORRECT SPOT OR SOMETHING ELSE? 
-## MAYBE THIS HAS TO DO WITH THE NEW DATA NOT BEING 'SUPPORTED' WITH THE PREDICT FUNCTION?
-## NOT SURE HOW ELSE WE CAN SHOW THE FITTED LINE
-# You can also predict to new data and that way see the fitted line
-
-#PREMON
-premon_try <- (predict(results$PREMON$RE_none, 
-                       newdata=data.frame(Moisture=seq(0,50, length.out = 51), type='expected',
-                                          densiometer=rep(
-                                            mean(seedlingdata$densiometer
-                                                 [seedlingdata$species=="PREMON"]), 51),
-                                          May_LA=rep(
-                                            mean(seedlingdata$May_LA
-                                                 [seedlingdata$species=="PREMON"], na.rm=T), 51))))
-
-##TAKE THE NEG EXPONENT
-Premon_predict<- exp(-premon_predict)
-Urebac_predict<- exp(-urebac_predict)
-
-
-##THE POINTS SEEM CORRECT BUT THE LINE ISN'T CORRECT
-plot(seq(0,50, length.out = 51), Test, type='l', ylim=c(0,1))
-test2 <- exp(-predict(results$PREMON$RE_none, type='expected'))
-points(moist, test2, col=4)
-
-plot(seq(0,50, length.out = 51), premon_predict, ylim=c(0,1))
-test2 <- exp(-predict(results$PREMON$RE_none, type='expected'))
-points(moist, test2, col=4)
-
-
-Premon_predict <- exp(-predict(results$PREMON$RE_none, type='expected'))
-Urebac_predict <- exp(-predict(results$UREBAC$RE_none, type='expected'))
-Manbid_predict<- exp(-predict(results$MANBID$RE_none, type='expected'))
-Tetbal_predict <- exp(-predict(results$TETBAL$RE_none, type='expected'))
-Inlau_predict<- exp(-predict(results$INLAU$RE_none, type='expected'))
-Guagui_predict<- exp(-predict(results$GUAGUI$RE_none, type='expected'))
-
-
-plot(PREMON_moist, Premon_predict, col=1, pch = 19, ylab="Survival probability", xlab="Soil moisture",ylim=c(0,1) )
-points(UREBAC_moist, Urebac_predict, col=2, pch = 19 )
-points(MANBID_moist, Manbid_predict, col=3, pch = 19 )
-points(TETBAL_moist, Tetbal_predict, col=4, pch = 19 )
-points(INLAU_moist, Inlau_predict, col=5, pch = 19 )
-points(GUAGUI_moist, Guagui_predict, col=6, pch = 19 )
-legend(35, 0.4, legend=c("PREMON", "UREBAC", "MANBID", "TETBAL", "INLAU", "GUAGUI"),
-       col=c("1", "2", "3", "4", "5", "6"), pch = 19)
-
-
-points(moist, Premon_predict, col=1)
-points(moist, Manbid_predict, col=3)
-points(moist, Tetbal_predict, col=4)
-points(moist, Inlau_predict, col=5)
-
-
-
-
-
-# Note there that the default 'type' of prediction for the cox model is "lp", which
-# apparently means 'linear predictor'.  There are other options; see these help files:
-?predict.coxph # This allows for 'newdata' and different 'types'
-?predict.coxme # This says 'newdata' is not yet supported but it allows for different 'types' of predictions
-
+write.csv(t1, "Desktop/Table1.csv", row.names = F)
 
 
 
@@ -152,72 +86,200 @@ points(moist, Inlau_predict, col=5)
 ##############################################
 ##############################################
 
+# The original growth data has plant size at each census.
+# We first create the new data.frame to have growth during each interval.
 
-# Here is the growth script and data, which hasn’t been changed since you so expertly crafted it. The script needs to be modified so the coefficients (and 95% CI) for soil moisture, densitometer & starting size are saved for each species from the best model to be used in plotting with the PCA scores.  
-
-
-# setwd("C:\\Users\\Matlaga\\Documents\\R")
-
-# For Bob
-# setwd("/Users/au529793/Downloads/fw")
-
-library(lme4)
-
-d <- read.table("Seedling growth june 12.txt",header=TRUE,sep="\t")
-
-# Create the new data.frame to have growth during each interval
-
-df <- data.frame()
-for(i in unique(d$Order)){
-  focdat <- d[d$Order==i,]
+growdf <- data.frame()
+for(i in unique(grow$Order)){
+  focdat <- grow[grow$Order==i,]
   species <- as.factor(focdat$Species[-1])
-  growth <- (focdat$Leaf_area[-1] - focdat$Leaf_area[-9]) / (focdat$days[-1] - focdat$days[-9])
+  growth <- (focdat$Leaf_area[-1] - focdat$Leaf_area[-9]) / (focdat$Days[-1] - focdat$Days[-9])
   startsize <- focdat$Leaf_area[-9]
-  # Note here we are using the grand mean soil moisture...
   moisture <- focdat$Moisture[-1]
-  densiometer <- focdat$densiometer[-1]
+  densiometer <- focdat$Densiometer[-1]
   plot <- as.factor(focdat$Shelter[-1])
   indv <- as.factor(focdat$Order[-1])
   interval <- 1:8
   tmpdf <- data.frame(species, growth, moisture, densiometer, startsize, plot, indv, interval)
-  df <- rbind(df, tmpdf)
-  df <- df[!is.na(df$growth),]
+  growdf <- rbind(growdf, tmpdf)
+  growdf <- growdf[!is.na(growdf$growth),]
 }
 
+# Fit the growth models
+grow_fits <- list()
 
-cols <- RColorBrewer::brewer.pal(8, "Dark2")
-plot(df$moisture, df$growth, col=scales::alpha(cols[df$species],0.5), pch=16)
-abline(h=0, lty=2)
+# grow_fits0 <- list()
+# grow_fits1 <- list()
+# grow_fits2 <- list()
+# grow_fits3 <- list()
+# grow_fits4 <- list()
 
-fits <- list()
-for(sp in 1:length(levels(df$species))){
-  print(levels(df$species)[sp])
-  tmpdf <- df[df$species %in% levels(df$species)[sp],]
-  head(tmpdf)
-  fits[[sp]] <- lmer(growth ~ moisture + densiometer + startsize + (1|plot) + (1|indv),
-                     data=tmpdf)
-  names(fits)[sp] <- levels(df$species)[sp]
+for(sp in 1:length(levels(growdf$species))){
+  
+  print(levels(growdf$species)[sp])
+
+  tmpdf <- growdf[growdf$species %in% levels(growdf$species)[sp],]
+  
+  # grow_fits0[[sp]] <- lmer(growth ~ moisture + densiometer + startsize + (1|plot) + (1|indv), 
+  #                          data=tmpdf)
+  # 
+  # grow_fits1[[sp]] <- lmer(growth ~ moisture + I(moisture^2) + densiometer + startsize 
+  #                          + (1|plot) + (1|indv), data=tmpdf)
+  # 
+  # grow_fits2[[sp]] <- lmer(growth ~ moisture + densiometer + startsize + (1|plot),
+  #                         data=tmpdf)
+  # 
+  # grow_fits3[[sp]] <- lm(growth ~ moisture + densiometer + startsize, 
+  #                        data=tmpdf)
+  # 
+  grow_fits[[sp]] <- lm(growth ~ moisture + densiometer + startsize, 
+                         data=tmpdf)
+  
+  names(grow_fits)[sp] <- levels(growdf$species)[sp]
 }
 
-# Extract the coefficient for soil moisture
-moist_coeffs <- unlist(lapply(fits, function(x) fixef(x)[2]))
+# moist_coeffs <- unlist(lapply(grow_fits, function(x) fixef(x)[2]))
 
-# Get 95% confidence intervals on the soil moisture effect
-moist_coeffs_cis <- do.call(rbind, lapply(fits, function(x) confint(x)['moisture',]))
+# round(do.call(cbind, lapply(grow_fits, function(x) fixef(x))),3)
 
-# Plot the point estimates with 95% CIs
-par(mar=c(6,5,2,2))
-plot(moist_coeffs, axes=F, xlab=NA, 
-     ylab='Estimated coefficient (growth x moisture)', 
-     pch=21, bg=cols, cex=2, ylim=range(cis))
-segments(1:8, cis[,1], 1:8, cis[,2], col=cols, lwd=2)
-abline(h=0, lty=2)
-axis(1, labels=levels(df$species), at=1:8, las=2)
-axis(2)
-box()
+# Extract coefficients and compute confidence intervals
+Species <- rep(names(grow_fits), each=4)
+Variable <- rep(c("Intercept","Moisture","Densiometer","Start_size"), 8)
+# Estimate <- round(do.call(c, lapply(grow_fits, fixef)),3)
+Estimate <- round(do.call(c, lapply(grow_fits, coef)),3)
+# cis <- do.call(rbind, lapply(grow_fits, function(x) round(confint(x)[-c(1:3),],3)))
+cis <- do.call(rbind, lapply(grow_fits, function(x) round(confint(x),3)))
 
+t2 <- as.data.frame(cbind(Species, Variable, Estimate, cis))
+rownames(t2) <- NULL
+t2
 
 
+
+### Plot the point estimates with 95% CIs
+# par(mar=c(6,5,2,2))
+# plot(moist_coeffs, axes=F, xlab=NA, 
+#      ylab='Estimated coefficient (growth x moisture)', 
+#      pch=21, bg=cols, cex=2, ylim=range(cis))
+# segments(1:8, cis[,1], 1:8, cis[,2], col=cols, lwd=2)
+# abline(h=0, lty=2)
+# axis(1, labels=levels(growdf$species), at=1:8, las=2)
+# axis(2)
+# box()
+
+
+###############################################
+### Plotting Effects on Survival and Growth ###
+###############################################
+
+pdf("/Users/au529793/Desktop/Figure1.pdf", width = 6, height = 8)
+
+# Predict survival as a function of soil moisture by species
+pred_moist <- list()
+pred_light <- list()
+pred_size <- list()
+
+for (i in seq_along(levels(surv$Species))){
+  focdat <- surv[surv$Species %in% levels(surv$Species)[i],]
+  newdata_moist <- data.frame(Moisture = 0:50, 
+                              Densiometer=mean(focdat$Densiometer),
+                              May_LA=mean(focdat$May_LA, na.rm=T), Days=228, Status=1)
+  pred_moist[[i]] <- exp(-predict(surv_fits_plot[[i]], newdata=newdata_moist, type='expected'))
+  newdata_light <- data.frame(Moisture=mean(focdat$Moisture),
+                              Densiometer=0:25,
+                              May_LA=mean(focdat$May_LA, na.rm=T), Days=228, Status=1)
+  pred_light[[i]] <- exp(-predict(surv_fits_plot[[i]], newdata=newdata_light, type='expected'))
+  newdata_size <- data.frame(Moisture=mean(focdat$Moisture),
+                             Densiometer=mean(focdat$Densiometer),
+                             May_LA=seq(min(surv$May_LA, na.rm=T), 
+                                        max(surv$May_LA, na.rm=T), 
+                                        length.out = 51), Days=228, Status=1)
+  pred_size[[i]] <- exp(-predict(surv_fits_plot[[i]], newdata=newdata_size, type='expected'))
+}
+
+par(mfcol=c(3,2), mar=c(4,5,1,0.5), oma=c(1,1,3,1))
+
+plot(0:50, ylim=c(0,100), pch=NA,
+     xlab="Soil Moisture (%)", 
+     ylab="Predicted Survival (%)")
+mtext("Survival", 3, 1)
+for(i in 1:length(pred_moist)){
+  lty <- ifelse(t1$p[t1$Variable=="Moisture"][i]<0.05, 1, 2)
+  lwd <- ifelse(t1$p[t1$Variable=="Moisture"][i]<0.05, 3, 2)
+  points(0:50, 100*pred_moist[[i]], col=cols[i], type='l', lwd=lwd, lty=lty)
+}
+
+plot(0:25, ylim=c(0,100), pch=NA,
+     xlim=c(-0.1,25), xlab="Canopy openness (%)", 
+     ylab="Predicted Survival (%)")
+for(i in 1:length(pred_light)){
+  lty <- ifelse(t1$p[t1$Variable=="Densiometer"][i]<0.05, 1, 2)
+  lwd <- ifelse(t1$p[t1$Variable=="Densiometer"][i]<0.05, 3, 2)
+  points(0:25, 100*pred_light[[i]], col=cols[i], type='l', lwd=lwd, lty=lty)
+}
+
+plot(1,1, xlim=range(surv$May_LA, na.rm=T), 
+     ylim=c(0,100), pch=NA,
+     xlab=bquote("Initial leaf area (cm"^2*")"), 
+     ylab="Predicted Survival (%)")
+for(i in 1:length(pred_size)){
+  x <- seq(min(surv$May_LA, na.rm=T), 
+           max(surv$May_LA, na.rm=T), 
+           length.out = 51)
+  lty <- ifelse(t1$p[t1$Variable=="May_LA"][i]<0.05, 1, 2)
+  lwd <- ifelse(t1$p[t1$Variable=="May_LA"][i]<0.05, 3, 2)
+  points(x, 100*pred_size[[i]], col=cols[i], type='l', lwd=lwd, lty=lty)
+}
+legend('bottomright', legend=levels(surv$Species),
+       cex=0.7, bty='n', lty=1, col=cols, lwd=2)
+
+plot(0:50, ylim=c(-0.5,1), pch=NA,
+     xlab="Soil Moisture (%)", 
+     ylab=bquote("Pred. Growth (cm"^2~"day"^-1*")"))
+for(i in seq_along(levels(growdf$species))){
+  tmpdf <- growdf[growdf$species == levels(growdf$species)[i],]
+  newdata <- data.frame(moisture=0:50,
+                        densiometer=mean(tmpdf$densiometer),
+                        startsize=mean(tmpdf$startsize))
+  lty <- ifelse(summary(grow_fits[[i]])$coeff["moisture", 4] < 0.05, 1, 2)
+  lwd <- ifelse(summary(grow_fits[[i]])$coeff["moisture", 4] < 0.05, 3, 2)
+  pred <- predict(grow_fits[[i]], newdata=newdata, re.form=NA)
+  points(0:50, pred, col=cols[i], type='l', lwd=lwd, lty=lty)
+}
+legend('topleft', legend=levels(surv$Species),
+       cex=0.7, bty='n', lty=1, col=cols, lwd=2)
+mtext("Growth", 3, 1)
+
+
+plot(0:25, ylim=c(-0.25,1), pch=NA,
+     xlab="Canopy openness (%)", 
+     ylab=bquote("Pred. Growth (cm"^2~"day"^-1*")"))
+for(i in seq_along(levels(growdf$species))){
+  tmpdf <- growdf[growdf$species == levels(growdf$species)[i],]
+  newdata <- data.frame(moisture=mean(tmpdf$moisture),
+                        densiometer=0:25,
+                        startsize=mean(tmpdf$startsize))
+  lty <- ifelse(summary(grow_fits[[i]])$coeff["densiometer", 4] < 0.05, 1, 2)
+  lwd <- ifelse(summary(grow_fits[[i]])$coeff["densiometer", 4] < 0.05, 3, 2)
+  pred <- predict(grow_fits[[i]], newdata=newdata, re.form=NA)
+  points(0:25, pred, col=cols[i], type='l', lwd=lwd, lty=lty)
+}
+
+plot(0:50, ylim=c(-1,1.5), pch=NA,
+     xlab=bquote("Initial leaf area (cm"^2*")"), 
+     ylab=bquote("Pred. Growth (cm"^2~"day"^-1*")"))
+for(i in seq_along(levels(growdf$species))){
+  tmpdf <- growdf[growdf$species == levels(growdf$species)[i],]
+  newdata <- data.frame(moisture=mean(tmpdf$moisture),
+                        densiometer=mean(tmpdf$densiometer),
+                        startsize=0:50)
+  lty <- ifelse(summary(grow_fits[[i]])$coeff["startsize", 4] < 0.05, 1, 2)
+  lwd <- ifelse(summary(grow_fits[[i]])$coeff["startsize", 4] < 0.05, 3, 2)
+  pred <- predict(grow_fits[[i]], newdata=newdata, re.form=NA)
+  points(0:50, pred, col=cols[i], type='l', lwd=lwd, lty=lty)
+}
+
+dev.off()
 
 
 
