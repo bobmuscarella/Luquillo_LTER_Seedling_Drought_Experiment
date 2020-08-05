ca4ff5e48b4bd64e6dcfbbbb692bff8f7d1b67c1
BobMuscarella <bob.muscarella@gmail.com>
2020-08-05 09:48:06 +0200

    Update Combined_script.R

--- Code/Combined_script.R
+++ Code/Combined_script.R
@@ -1,8 +1,6 @@
 ###########################################################################
 ###########################################################################
-###########################################################################
 ### Combined code for Seedling Drought Experiment #########################
-###########################################################################
 ###########################################################################
 ###########################################################################
 
@@ -10,7 +8,21 @@
 ### General setup ###
 #####################
 
-# Read data from Github
+### LOAD PACKAGES
+require(survival)
+require(survminer)
+require(coxme)
+require(ggplot2)
+require(RColorBrewer)
+require(lme4)
+require(scales)
+require(ggplot2)
+require(factoextra)
+require(vegan)
+require(plotfunctions)
+
+
+### READ DATA FROM GITHUB
 path <- "https://raw.github.com/bobmuscarella/Luquillo_LTER_Seedling_Drought_Experiment/master/Data/"
 
 grow <- read.csv(paste0(path, "LUQ_DroughtExp_Seedling_growth.csv"))
@@ -31,52 +43,12 @@ nutrient <- read.csv(paste0(path, "LUQ_DroughtExp_Seedling_leafnutrients.csv"))
 nutrient$Species <- factor(nutrient$Species, levels = levels(grow$Species))
 nutrient$ID <- paste(nutrient$Plot, nutrient$Position, sep = '.')
 
-# Load packages
-require(survival)
-require(survminer)
-require(coxme)
-require(ggplot2)
-require(RColorBrewer)
-library(lme4)
-library(scales)
-library(ggplot2)
-library(factoextra)
-library(vegan)
-library(plotfunctions)
 
-# Set a color palette for species
+### SET A COLOR PALETTE TO IDENTIFY SPECIES
 cols <- brewer.pal(8, "Dark2")
 
-#########################
-### Survival analysis ###
-#########################
 
-### Run species-by-species survival models in a loop
-surv_fits <- surv_fits_plot <- list()
-options(na.action=na.exclude) # retain NA in predictions
-
-for (i in seq_along(levels(surv$Species))){
-  # Subset the data for the 'i-th' species
-  focdat <- surv[surv$Species %in% levels(surv$Species)[i],]
-  
-  ############################################################
-  ### EXCLUDE INDIVIDUALS WHO DIED IN THE FIRST 2 INTERVALS...
-  # focdat <- focdat[focdat$Days>14,]
-  ############################################################
-  
-  # Run the models with plot random effect (we tested other random effects but AIC selected this)
-  surv_fits[[i]] <- coxme(Surv(Days, Status) ~ Moisture + Densiometer + Start_LA
-                             + (1|Plot), data=focdat)
-  # Run models without random effect in order to be able to plot predicted response
-  # (not currently possible with coxme models)
-  surv_fits_plot[[i]] <- coxph(Surv(Days, Status) ~ Moisture + Densiometer + Start_LA, 
-                             data=focdat, model=T)
-  
-  # Name the items in the resulting list
-  names(surv_fits)[i] <- levels(surv$Species)[i]
-}
-
-### Function to extract summary table from Cox ME models
+### HELPER FUNCTION to extract summary table from Cox ME models
 extract_coxme_table <- function (mod){
   beta <- mod$coefficients
   nvar <- length(beta)
@@ -88,79 +60,14 @@ extract_coxme_table <- function (mod){
   return(table)
 }
 
-### Make a summary table for survival model results
-t1 <- do.call(rbind, lapply(surv_fits, extract_coxme_table))
-t1 <- round(t1, 3)
-t1$Species <- unlist(lapply(strsplit(rownames(t1), "\\."), function(x)x[[1]]))
-t1$Variable <- unlist(lapply(strsplit(rownames(t1), "\\."), function(x)x[[2]]))
-t1 <- cbind(t1, round(do.call(rbind, lapply(surv_fits, confint)),3))
-t1 <- t1[,c("Species","Variable","beta","2.5 %", "97.5 %","se","z","p")]
-
-write.csv(t1, "/Users/au529793/Projects/GIT/Luquillo_LTER_Seedling_Drought_Experiment/Tables/Table1_survival_summary.csv", row.names = F)
-
-
-##############################################
-### Growth analysis ##########################
-##############################################
-
-# The original growth data has plant size at each census.
-# We first create the new data.frame to have growth during each interval.
-
-growdf <- data.frame()
-for(i in unique(grow$Order)){
-  focdat <- grow[grow$Order==i,]
-  species <- as.factor(focdat$Species[-1])
-  growth <- (focdat$Leaf_area[-1] - focdat$Leaf_area[-9]) / (focdat$Days[-1] - focdat$Days[-9])
-  startsize <- focdat$Leaf_area[-9]
-  moisture <- focdat$Moisture[-1]
-  densiometer <- focdat$Densiometer[-1]
-  id <- focdat$ID[-1]
-  plot <- as.factor(focdat$Plot[-1])
-  indv <- as.factor(focdat$Order[-1])
-  interval <- 1:8
-  tmpdf <- data.frame(id, species, growth, moisture, densiometer, startsize, plot, indv, interval)
-  growdf <- rbind(growdf, tmpdf)
-  growdf <- growdf[!is.na(growdf$growth),]
-}
-
-# Fit the growth models
-grow_fits <- list()
-
-for(sp in 1:length(levels(growdf$species))){
-  print(levels(growdf$species)[sp])
-  tmpdf <- growdf[growdf$species %in% levels(growdf$species)[sp],]
-  
-  ############################################################
-  ### Remove the first two intervals to see how that affects the starting size effect
-  # tmpdf <- tmpdf[tmpdf$interval>1,]
-  ############################################################
-  
-  grow_fits[[sp]] <- lmer(growth ~ moisture + densiometer + startsize 
-                          + (1|plot), data=tmpdf)
-  names(grow_fits)[sp] <- levels(growdf$species)[sp]
-}
-
-# Extract coefficients and compute confidence intervals
-Species <- rep(names(grow_fits), each=4)
-Variable <- rep(c("Intercept","Moisture","Densiometer","Start_size"), 8)
-Estimate <- round(do.call(c, lapply(grow_fits, fixef)),3)
-growcis <- do.call(rbind, lapply(grow_fits, function(x) round(confint(x)[-(1:2),],3)))
-
-t2 <- as.data.frame(cbind(Species, Variable, Estimate, growcis))
-t2$`2.5 %` <- as.numeric(as.character(t2$`2.5 %`))
-t2$`97.5 %` <- as.numeric(as.character(t2$`97.5 %`))
-rownames(t2) <- NULL
-
-write.csv(t2, "/Users/au529793/Projects/GIT/Luquillo_LTER_Seedling_Drought_Experiment/Tables/Table2_growth_summary.csv", row.names = F)
-
-
 ######################################################################################
-### Plot individual growth trajectories over time with soil moisture and mortality ###
+### Figure of individual growth trajectories over time with soil moisture and mortality ###
 ######################################################################################
-
-grow$soil_col <- brewer.pal(10, "Spectral")[cut(grow$Moisture, 10)]
 
 pdf("/Users/au529793/Projects/GIT/Luquillo_LTER_Seedling_Drought_Experiment/Figures/Figure1.pdf", height=6, width=8)
+
+# Color based on soil moisture in each plot
+grow$soil_col <- brewer.pal(10, "Spectral")[cut(grow$Moisture, 10)]
 
 # Reorder the data for plotting convenience
 grow <- grow[order(grow$Species, grow$Order, grow$Census),]
@@ -208,14 +115,118 @@ for (sp in 1:length(unique(grow$Species))){
 dev.off()
 
 
+################################
+### Plot raw survival curves ###
+################################
+
+pdf("/Users/au529793/Projects/GIT/Luquillo_LTER_Seedling_Drought_Experiment/Figures/FigureS6.pdf", height=6, width=8)
+
+surv_curves_fit <- survfit(Surv(Days, Status) ~ Species, data = surv)  
+plot(surv_curves_fit, col=cols, lwd=2, axes=F, 
+     xlab="Days", ylab="Percent Surviving")
+legend('bottomleft', legend=levels(surv$Species),
+       cex=0.7, bty='n', lty=1, col=cols, lwd=2)
+axis(2, at=seq(0,1,by=0.2), labels=seq(0,100,by=20))
+axis(1)
+
+dev.off()
+
+
+#########################
+### Survival analysis ###
+#########################
+
+### Run species-by-species survival models in a loop
+surv_fits <- surv_fits_plot <- list()
+options(na.action=na.exclude) # retain NA in predictions
+
+for (i in seq_along(levels(surv$Species))){
+  # Subset the data for the ith species
+  focdat <- surv[surv$Species %in% levels(surv$Species)[i],]
+
+  # Run the models with plot random effect (we tested other random effects but AIC selected this)
+  surv_fits[[i]] <- coxme(Surv(Days, Status) ~ Moisture + Densiometer + Start_LA
+                             + (1|Plot), data=focdat)
+  # Run models without random effect in order to be able to plot predicted response
+  # (not currently possible with coxme models)
+  surv_fits_plot[[i]] <- coxph(Surv(Days, Status) ~ Moisture + Densiometer + Start_LA, 
+                             data=focdat, model=T)
+  
+  # Name the items in the resulting list
+  names(surv_fits)[i] <- levels(surv$Species)[i]
+}
+
+### Make a summary table for survival model results
+t1 <- do.call(rbind, lapply(surv_fits, extract_coxme_table))
+t1 <- round(t1, 3)
+t1$Species <- unlist(lapply(strsplit(rownames(t1), "\\."), function(x)x[[1]]))
+t1$Variable <- unlist(lapply(strsplit(rownames(t1), "\\."), function(x)x[[2]]))
+t1 <- cbind(t1, round(do.call(rbind, lapply(surv_fits, confint)),3))
+t1 <- t1[,c("Species","Variable","beta","2.5 %", "97.5 %","se","z","p")]
+
+write.csv(t1, "/Users/au529793/Projects/GIT/Luquillo_LTER_Seedling_Drought_Experiment/Tables/Table1_survival_summary.csv", row.names = F)
+
+
+##############################################
+### Growth analysis ##########################
+##############################################
+
+# The original growth data has plant size at each census.
+# We first create the new data.frame to have growth during each interval.
+
+growdf <- data.frame()
+for(i in unique(grow$Order)){
+  focdat <- grow[grow$Order==i,]
+  species <- as.factor(focdat$Species[-1])
+  growth <- (focdat$Leaf_area[-1] - focdat$Leaf_area[-9]) / (focdat$Days[-1] - focdat$Days[-9])
+  startsize <- focdat$Leaf_area[-9]
+  moisture <- focdat$Moisture[-1]
+  densiometer <- focdat$Densiometer[-1]
+  id <- focdat$ID[-1]
+  plot <- as.factor(focdat$Plot[-1])
+  indv <- as.factor(focdat$Order[-1])
+  interval <- 1:8
+  tmpdf <- data.frame(id, species, growth, moisture, densiometer, startsize, plot, indv, interval)
+  growdf <- rbind(growdf, tmpdf)
+  growdf <- growdf[!is.na(growdf$growth),]
+}
+
+# Fit the growth models
+grow_fits <- list()
+
+for(sp in 1:length(levels(growdf$species))){
+  print(levels(growdf$species)[sp])
+  tmpdf <- growdf[growdf$species %in% levels(growdf$species)[sp],]
+  
+  grow_fits[[sp]] <- lmer(growth ~ moisture + densiometer + startsize 
+                          + (1|plot), data=tmpdf)
+  
+  names(grow_fits)[sp] <- levels(growdf$species)[sp]
+}
+
+# Extract coefficients and compute confidence intervals
+Species <- rep(names(grow_fits), each=4)
+Variable <- rep(c("Intercept","Moisture","Densiometer","Start_size"), 8)
+Estimate <- round(do.call(c, lapply(grow_fits, fixef)),3)
+growcis <- do.call(rbind, lapply(grow_fits, function(x) round(confint(x)[-(1:2),],3)))
+
+t2 <- as.data.frame(cbind(Species, Variable, Estimate, growcis))
+t2$`2.5 %` <- as.numeric(as.character(t2$`2.5 %`))
+t2$`97.5 %` <- as.numeric(as.character(t2$`97.5 %`))
+rownames(t2) <- NULL
+
+write.csv(t2, "/Users/au529793/Projects/GIT/Luquillo_LTER_Seedling_Drought_Experiment/Tables/Table2_growth_summary.csv", row.names = F)
+
+
+
 ###########################################
-### Plot Effects on Survival and Growth ###
+### Figure Showing Effects on Survival and Growth ###
 ###########################################
 
 # Predict survival as a function of soil moisture by species
 pred_moist <- list()
-pred_light <- list()
-pred_size <- list()
+# pred_light <- list()
+# pred_size <- list()
 
 for (i in seq_along(levels(surv$Species))){
   focdat <- surv[surv$Species %in% levels(surv$Species)[i],]
@@ -223,16 +234,18 @@ for (i in seq_along(levels(surv$Species))){
                               Densiometer=mean(focdat$Densiometer),
                               Start_LA=mean(focdat$Start_LA, na.rm=T), Days=228, Status=1)
   pred_moist[[i]] <- exp(-predict(surv_fits_plot[[i]], newdata=newdata_moist, type='expected'))
-  newdata_light <- data.frame(Moisture=mean(focdat$Moisture),
-                              Densiometer=0:25,
-                              Start_LA=mean(focdat$Start_LA, na.rm=T), Days=228, Status=1)
-  pred_light[[i]] <- exp(-predict(surv_fits_plot[[i]], newdata=newdata_light, type='expected'))
-  newdata_size <- data.frame(Moisture=mean(focdat$Moisture),
-                             Densiometer=mean(focdat$Densiometer),
-                             Start_LA=seq(min(surv$Start_LA, na.rm=T), 
-                                        max(surv$Start_LA, na.rm=T), 
-                                        length.out = 51), Days=228, Status=1)
-  pred_size[[i]] <- exp(-predict(surv_fits_plot[[i]], newdata=newdata_size, type='expected'))
+  
+  ### Predictions across light and initial size (not used)
+  # newdata_light <- data.frame(Moisture=mean(focdat$Moisture),
+  #                             Densiometer=0:25,
+  #                             Start_LA=mean(focdat$Start_LA, na.rm=T), Days=228, Status=1)
+  # pred_light[[i]] <- exp(-predict(surv_fits_plot[[i]], newdata=newdata_light, type='expected'))
+  # newdata_size <- data.frame(Moisture=mean(focdat$Moisture),
+  #                            Densiometer=mean(focdat$Densiometer),
+  #                            Start_LA=seq(min(surv$Start_LA, na.rm=T), 
+  #                                       max(surv$Start_LA, na.rm=T), 
+  #                                       length.out = 51), Days=228, Status=1)
+  # pred_size[[i]] <- exp(-predict(surv_fits_plot[[i]], newdata=newdata_size, type='expected'))
 }
 
 
@@ -303,8 +316,7 @@ box()
 
 
 pt.pch <- ifelse(apply(t2[t2$Variable=="Moisture",c('2.5 %','97.5 %')]<0, 1, 
-                       function(x) x[1]==x[2]),
-                 16, 21)
+                       function(x) x[1]==x[2]), 16, 21)
 plot(as.numeric(as.character(t2$Estimate[t2$Variable=='Moisture'])), 
      axes=F, xlab=NA,
      ylab='Estimated coefficient',
